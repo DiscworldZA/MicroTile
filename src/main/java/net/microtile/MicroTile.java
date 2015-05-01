995c1cad041e05fccf54a62cbbf973b46c979f4d
ModdingChase <pd.van.lill@gmail.com>
2015-05-01 23:31:49 +0200

    Initial Commit.

---
+++ src/main/java/net/microtile/MicroTile.java
@@ -0,0 +1,20 @@
+package net.microtile;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MicroTile.MODID, name = MicroTile.MODNAME, version = MicroTile.VERSION)
+public class MicroTile
+{
+    public static final String MODID = "MT";    
+    public static final String MODNAME = "MicroTile";
+    public static final String VERSION = "0.0.1 Alpha";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
