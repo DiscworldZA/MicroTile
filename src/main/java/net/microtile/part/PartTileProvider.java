d882100484c192757251d816294dd589f53a5c78
ModdingChase <pd.van.lill@gmail.com>
2015-05-02 01:20:27 +0200

    update

--- src/main/java/net/microtile/part/PartTileProvider.java
+++ src/main/java/net/microtile/part/PartTileProvider.java
@@ -8,22 +8,25 @@ import codechicken.lib.vec.BlockCoord;
 import codechicken.multipart.MultipartGenerator;
 import codechicken.multipart.TMultiPart;
 
-public class PartTileProvider extends TMultiPart {
-
+public class PartTileProvider extends TMultiPart
+{
+	
 	private Block m_parent;
 	
 	@Override
-	public String getType() 
+	public String getType()
 	{
 		return MicroTile.MODID + "_multi_part_tile_provider";
 	}
 	
-	
-	
 	public boolean addPart(World world, BlockCoord pos, TMultiPart part)
 	{
-		if(world.isRemote) {System.out.print("Cannot add multi parts to a client tile."); return;}
-        return MultipartGenerator.addPart(world, pos, part);
+		if (world.isRemote)
+		{
+			System.out.print("Cannot add multi parts to a client tile.");
+			return false;
+		}
+		return this.addPart(world, pos, part);
 	}
-
+	
 }
