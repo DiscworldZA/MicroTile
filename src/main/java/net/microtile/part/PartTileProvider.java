8a18ec115edaa683efc812afb00a3eb97b9d108c
tim4242 <tim4242@gmx.de>
2015-05-03 00:48:01 +0200

    update

--- src/main/java/net/microtile/part/PartTileProvider.java
+++ src/main/java/net/microtile/part/PartTileProvider.java
@@ -1,19 +1,28 @@
 package net.microtile.part;
 
 import net.microtile.MicroTile;
+import net.microtile.tile.TileMultiTileProvider;
+import net.minecraft.block.Block;
 import codechicken.multipart.TMultiPart;
 
 public class PartTileProvider extends TMultiPart {
 
+	private Block m_parent;
+	
 	@Override
 	public String getType() 
 	{
 		return MicroTile.MODID + "_multi_part_tile_provider";
 	}
 	
+	
+	
 	public void onAdded()
 	{
-		
+		if(!(this.tile() instanceof TileMultiTileProvider))
+		{
+			
+		}
 	}
 
 }
